36de4a5cb8504763a1af252dc7d5b4ea7b714c3a
gaurav.pawar <pawargaurav1256@gmail.com>
2025-09-27 16:38:12 +0530

    created login and register pages

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Dashboard from './Components/Dashboard/Dashboard';
 import Header from './Components/Header/Header';
+import Register from './Components/Register/Register';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Dashboard></Dashboard>
-      
+      {/* <Dashboard></Dashboard> */}
+      {/* <Register></Register> */}
+      <Login></Login>
     </div>
   );
 }
