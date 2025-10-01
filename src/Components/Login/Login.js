76796ad76d7eaf85a5a95ff2e3c4c3bab42bc3bf
gaurav.pawar <pawargaurav1256@gmail.com>
2025-10-01 15:35:58 +0530

    called api for login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,31 @@
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
+import { useState } from "react";
+import axios from "axios";
 function Login(){
+    const[userLoginData, setuserLoginData]= useState({email:"",password:""});
+    function useremail(event){
+        let user={...userLoginData}
+        user["email"]=event.target.value;
+        setuserLoginData(user);
+    }
+    function userpassword(event){
+        let user={...userLoginData}
+        user["password"]=event.target.value;
+        setuserLoginData(user);
+    }
+    function handleLoginData(){
+        axios.get("http://localhost:4200/user",userLoginData)
+            .then((Response)=>{
+                console.log(Response);
+                navigate('/blogs')
+            })
+        
+    }
     const navigate =useNavigate();
-     function handleLoginButton(){
-        navigate('/blogs')
-     }
+    //  function handleLoginButton(){
+        
+    //  }
     return(
         <div>
             <div className="loginsection">
@@ -13,10 +34,10 @@ function Login(){
                 <hr />
                 <div className="registername">Login</div>
                 <label>Email</label><br />
-                <input type="email" placeholder="test@gmail.com"  className="inputfielddata" /><br />
+                <input type="email" placeholder="test@gmail.com"  className="inputfielddata" value={userLoginData.email} onChange={useremail} /><br />
                 <label>Password</label><br />
-                <input type="password" placeholder="password"  className="inputfielddata" /><br />
-                <button className="loginbutton" onClick={handleLoginButton}>Login</button>
+                <input type="password" placeholder="password"  className="inputfielddata" value={userLoginData.password} onChange={userpassword} /><br />
+                <button className="loginbutton" onClick={handleLoginData}>Login</button>
             </div>
 
         </div>
