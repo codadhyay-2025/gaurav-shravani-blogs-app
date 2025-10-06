3e04ce5394a39f8284c3bde8f312caf683cffea1
gaurav.pawar <pawargaurav1256@gmail.com>
2025-10-06 15:21:23 +0530

    api called for delete button

--- src/Components/BlogsDashboard/BlogsDashboard.js
+++ src/Components/BlogsDashboard/BlogsDashboard.js
@@ -1,21 +1,56 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import "./BlogsDashboard.css"
+import axios from "axios";
+import { useEffect, useState } from "react";
 function BlogsDashboard() {
     const navigate = useNavigate();
-    
+    const {id}=useParams();
+    const[blogs,setblogs]= useState();
     function handleCreateNewPostButton(){
         navigate('/createpost')
     }
     function handleEditButton(){
         navigate('/createpost')
     }
-    const arr=[{
-        title:"hello",
-        created_by:"grv",
-        created_At:"12/2/2001",
-        description:"hello i am gaurav"
+    // const arr=[{
+    //     title:"hello",
+    //     created_by:"grv",
+    //     created_At:"12/2/2001",
+    //     description:"hello i am gaurav"
+    // }];
+    function jsondata(){
+        axios.get("http://localhost:4200/blogs")
+        .then((res)=>{
+            setblogs(res.data);
+            
+        })
+        .catch((error)=>{
+            console.log("error get blogs",error);
+            
+        })
+    }
+    useEffect(()=>{
+        // axios.get("http://localhost:4200/blogs",blogs)
+        // .then((Response)=>{
+        //     setblogs(Response.data);
+            // console.log(Response);
+            jsondata();
+        // })
+    },[])
+    function handleDeleteButton(id){
+        axios.delete("http://localhost:4200/blogs/"+id)
+        .then((Response)=>{
+            console.log(Response);
+            
+            alert("blog deleted")
+            jsondata();
+        })
+        .catch((error)=>{
+            console.error("error deleting blog",error);
+            
+        })
+    }
 
-    }];
     
     return (
         <div>
@@ -30,7 +65,7 @@ function BlogsDashboard() {
                 </div>
                 <hr />
                 
-                {arr.map((singleElement)=>{
+                {blogs?.map((singleElement)=>{
                 return <div className="blogSection">
                     <div className="blogTitle">{singleElement.title}</div>
                     <div><strong>Created By</strong>{singleElement.created_by}</div>
@@ -46,7 +81,7 @@ function BlogsDashboard() {
                         </div>
                         <div>
                             <button className="edit" onClick={handleEditButton}><i class="fa fa-pencil" aria-hidden="true"> Edit </i></button>
-                            <button className="delete"><i class="fa fa-trash" aria-hidden="true"> Delete </i></button>
+                            <button className="delete" onClick={()=>handleDeleteButton(singleElement.id)}><i class="fa fa-trash" aria-hidden="true"> Delete </i></button>
                         </div>
                     </div>
                 </div>})}
